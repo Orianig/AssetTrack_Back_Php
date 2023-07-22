6948084cf5c0b963664c92a5274e738a044d5ab5
orianig <oriana.ig016@gmail.com>
2023-07-22 15:42:57 +0200

    feat: add reservation to provider

--- app/Providers/EventServiceProvider.php
+++ app/Providers/EventServiceProvider.php
@@ -18,6 +18,10 @@ class EventServiceProvider extends ServiceProvider
         Registered::class => [
             SendEmailVerificationNotification::class,
         ],
+
+        \App\Events\BookingUpdated::class => [
+            \App\Observers\BookingObserver::class,
+        ],
     ];
 
     /**
