99d9027037b03961bbc430f0cd3604d8c355c518
orianig <oriana.ig016@gmail.com>
2023-07-22 15:42:08 +0200

    feat: historicalBooking model

--- app/Models/HistoricalBooking.php
+++ app/Models/HistoricalBooking.php
@@ -8,4 +8,10 @@ use Illuminate\Database\Eloquent\Model;
 class HistoricalBooking extends Model
 {
     use HasFactory;
+   
+    public function booking()
+    
+    {
+        return $this->belongsTo(AssetsUser::class, 'user_id', 'user_id')->where('asset_id', $this->asset_id);
+    }
 }
