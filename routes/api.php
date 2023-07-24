210c772c48fd855b6a61c7b88e4412ac4ed2a333
orianig <oriana.ig016@gmail.com>
2023-07-24 16:08:28 +0200

    feat: add get profile route

--- routes/api.php
+++ routes/api.php
@@ -1,10 +1,12 @@
 <?php
 
-use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\DB;
 
+//CONTROLLERS
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\UserController;
 
 /*
 |--------------------------------------------------------------------------
@@ -28,7 +30,7 @@ Route::post('/register', [AuthController::class, 'register']);
 Route::post('/login', [AuthController::class, 'login']);
 
 //USER CONTROLLER
-
+Route::get('/profile', [UserController::class, 'profile'])->middleware(['auth:sanctum']);
 
 // TEAMS CONTROLLER
 
