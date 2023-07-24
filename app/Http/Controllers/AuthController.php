18d84865ca77f28a309d967250254f1c90b9f271
orianig <oriana.ig016@gmail.com>
2023-07-24 12:30:21 +0200

    feat: add register function - create user

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,17 +2,66 @@
 
 namespace App\Http\Controllers;
 
+//necesary facades - class
+
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Validation\Rules\Password;
+use Laravel\Sanctum\PersonalAccessToken;
+use Symfony\Component\HttpFoundation\Response;
+
+//models references
+use App\Models\User;
 
 class AuthController extends Controller
 {
-    public function index()
+
+    //REGISTER
+
+    public function register(Request $request)
     {
-        return "it is working";
+        try {
+
+            //data validation
+            $validator = Validator::make($request->all(), [
+                'name' => 'required|string',
+                'surname' => 'required|string',
+                'email' => 'required|email|unique:users,email',
+                'password' => ['required', Password::min(8)->mixedCase()->numbers()]
+            ]);
+
+            if ($validator->fails()) {
+                return response()->json($validator->errors(), 400);
+            }
+
+            $validData = $validator->validated();
+
+            //creation new user 
+            $newUser = User::create([
+                'name' => $validData['name'],
+                'surname' => $validData['surname'],
+                'email' => $validData['email'],
+                'password' => bcrypt($validData['password']),
+                'role_id' => 3
+            ]);
+
+            //Token generation
+            $token = $newUser->createToken('apiToken')->plainTextToken;
+
+            return response()->json([
+                'message' => 'User registered successfully',
+                'data' => $newUser,
+                'token' => $token
+            ], Response::HTTP_CREATED);
+        } catch (\Throwable $th) {
+            Log::error('Error registering user ' . $th->getMessage());
+
+            return response()->json([
+                'message' => 'Error registering user'
+            ], Response::HTTP_INTERNAL_SERVER_ERROR);
+        }
     }
 
-    public function index1($id)
-    {
-        return "it is working the number " . $id;
-    }
 }
