b23a385c742ea241fadbecfed6cd4d2a4f36ba7f
orianig <oriana.ig016@gmail.com>
2023-08-02 19:52:43 +0200

    feat: add fillable caracteristic

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -9,6 +9,15 @@ class Project extends Model
 {
     use HasFactory;
 
+    protected $fillable = [
+        'name', // Agrega el campo "name" a la propiedad $fillable
+        'place',
+        'start_date',
+        'end_date',
+        'team',
+        'description',
+    ];
+    
     public function inventory()
     {
         return $this->belongsToMany(Inventory::class, 'inventory_projects', 'project_id', 'inventory_id')
