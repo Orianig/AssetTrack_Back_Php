561fb11499ee0cd55ba137fdd66897a16c158035
orianig <oriana.ig016@gmail.com>
2023-08-02 19:53:01 +0200

    feat: add endpoints

--- routes/api.php
+++ routes/api.php
@@ -3,12 +3,12 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-
 //CONTROLLERS
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\ProjectController;
 use App\Http\Controllers\TeamController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -34,13 +34,22 @@ Route::post('/auth/login', [AuthController::class, 'login']);
 //USER CONTROLLER
 Route::get('/user/profile', [UserController::class, 'getUserProfile'])->middleware(['auth:sanctum']);
 Route::put('/user/profile/updateProfile', [UserController::class, 'updateProfile'])->middleware(['auth:sanctum']);
-Route::delete('user/deleteUser', [UserController::class, 'deleteUser'])->middleware(['auth:sanctum', 'isAdmin']);
+Route::get('/user/allUsers', [UserController::class, 'getAllUsers'])->middleware(['auth:sanctum', 'isAdmin']);
+// Route::delete('/user/deleteUser', [UserController::class, 'deleteUser'])->middleware(['auth:sanctum', 'isAdmin']);
 
 // TEAMS CONTROLLER
 Route::get('/user/teams', [TeamController::class, 'getUserTeams'])->middleware(['auth:sanctum']);
+Route::get('/team/allTeams', [TeamController::class, 'getAllTeams'])->middleware(['auth:sanctum']);
+Route::post('/team/newTeam', [TeamController::class, 'createTeam'])->middleware(['auth:sanctum', 'isAdmin']);
+Route::put('/team/updateTeam', [TeamController::class, 'updateTeam'])->middleware(['auth:sanctum', 'isAdmin']);
+// Route::delete('/team/deleteTeam', [TeamController::class, 'deleteTeam'])->middleware(['auth:sanctum', 'isAdmin']);
 
 //PROJECT CONTROLLER
 Route::get('/user/projects', [ProjectController::class, 'getUserProjects'])->middleware(['auth:sanctum']);
+Route::get('/project/allProjects', [ProjectController::class, 'getAllProjects'])->middleware(['auth:sanctum']);
+Route::post('/project/newProject', [ProjectController::class, 'createProject'])->middleware(['auth:sanctum','isAdmin']);
+Route::put('/project/updateProject', [ProjectController::class, 'updateProject'])->middleware(['auth:sanctum','isAdmin']);
+Route::delete('/project/deleteProject', [ProjectController::class, 'deleteProject'])->middleware(['auth:sanctum','isAdmin']);
 
 //INVENTORY CONTROLLER
 
