c0e0bfa957d2b0da02460de255f4823e827cce98
orianig <oriana.ig016@gmail.com>
2023-08-02 17:52:11 +0200

    feat: add controllers

--- routes/api.php
+++ routes/api.php
@@ -7,6 +7,8 @@ use Illuminate\Support\Facades\Route;
 //CONTROLLERS
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\ProjectController;
+use App\Http\Controllers\TeamController;
 
 /*
 |--------------------------------------------------------------------------
@@ -32,13 +34,13 @@ Route::post('/auth/login', [AuthController::class, 'login']);
 //USER CONTROLLER
 Route::get('/user/profile', [UserController::class, 'getUserProfile'])->middleware(['auth:sanctum']);
 Route::put('/user/profile/updateProfile', [UserController::class, 'updateProfile'])->middleware(['auth:sanctum']);
-Route::delete('/deleteUser', [UserController::class, 'deleteUser'])->middleware(['auth:sanctum', 'isAdmin']);
+Route::delete('user/deleteUser', [UserController::class, 'deleteUser'])->middleware(['auth:sanctum', 'isAdmin']);
 
 // TEAMS CONTROLLER
-
+Route::get('/user/teams', [TeamController::class, 'getUserTeams'])->middleware(['auth:sanctum']);
 
 //PROJECT CONTROLLER
-
+Route::get('/user/projects', [ProjectController::class, 'getUserProjects'])->middleware(['auth:sanctum']);
 
 //INVENTORY CONTROLLER
 
